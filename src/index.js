241a2c6904d88ed043a3a23741f21048774fb7d4
Sanket Khonde <sanketkhonde30@gmail.com>
2024-12-30 14:28:20 +0530

    added roles and user_roles through tables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { PORT, JWT_KEY } = require("./config/serverConfig");
+const { PORT, DB_SYNC } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
 
 const Router = require("./router/index");
-const bcrypt = require("bcrypt");
-const { user } = require("./models/index");
+const db = require("./models/index");
+// const bcrypt = require("bcrypt");
+// const { user } = require("./models/index");
 // const UserRepository = require("./repository/user-repository");
-const UserService = require("./services/user-service");
+// const UserService = require("./services/user-service");
+// const { DB_SYNC } = require("./config");
 
 const prepareAndStartServer = async () => {
   //
@@ -18,6 +20,16 @@ const prepareAndStartServer = async () => {
   app.use("/api", Router);
   //
   app.listen(PORT, async () => {
+    if (DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+
+    const u1 = await db.user.findByPk(2);
+    const r1 = await db.Role.findByPk(1);
+
+    // u1.addRole(r1);
+    // const response = await u1.getRoles();
+    // console.log(response);/
     console.log("Server started on the PORT ", PORT);
   });
 };
--- src/router/v1/index.js
+++ src/router/v1/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const UserController = require("../../controllers/user-controller");
-const AuthRequestValidatorMiddleware = requestAnimationFrameWE;
 
 const router = express.Router();
 
